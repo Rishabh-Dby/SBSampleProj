25974896f0eb939da45006752eaaa52843a5ea1b
Rishabh_Dby <rishabhdy.7@gmail.com>
2024-05-27 00:14:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 module.exports = {
   platform: 'github',
   onboarding: true,
+  token: process.env.GITHUB_TOKEN,
   repositories: [
     'Rishabh-Dby/SBSampleProj',
   ],
